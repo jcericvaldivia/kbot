5a84c2c1d6d49645bb75dfa22f34ffeb068b2718
Eric Valdivia <jcericvaldivia@gmail.com>
2020-10-23 02:22:05 +0000

    Funcion multiplicacion total

--- factura.rb
+++ factura.rb
@@ -1,4 +1,17 @@
+puts 10*5
+
+class Factura
+    
+    def calculoTotal(cantidad, precioUnitario)
+        total = cantidad * precioUnitario
+    end 
+
+
+end
+
 puts "hola, mundo"
+
+
 
 cantidad = ARGV[0]
 precioUnitario = ARGV[1]
